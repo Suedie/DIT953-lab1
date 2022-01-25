a1926ef40c3634de0d55cb1d9bb3128533691f51
Suedie <elias.haqiqat@protonmail.com>
2022-01-25 12:20:12 +0100

    Subclassing and interfacing

---
+++ Car.java
@@ -0,0 +1,53 @@
+import java.awt.*;
+
+public abstract class Car {
+
+    private int nrDoors; // Number of doors on the car
+    private double enginePower; // Engine power of the car
+    private double currentSpeed; // The current speed of the car
+    private Color color; // Color of the car
+    private String modelName; // The car model name
+
+    //Base constructor
+    public Car(int nrDoors, double enginePower, Color color, String modelName){
+        this.nrDoors = nrDoors;
+        this.enginePower = enginePower;
+        this.color = color;
+        this.modelName = modelName;
+    }
+
+    // Getters and Setters
+    public int getNrDoors(){
+        return nrDoors;
+    }
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+        color = clr;
+    }
+
+    public void setCurrentSpeed(double speed) {
+        currentSpeed = speed;
+    }
+
+    public void startEngine(){
+        currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+        currentSpeed = 0;
+    }
+
+
+
+}
